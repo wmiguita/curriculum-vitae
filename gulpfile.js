98ba497f6ae373a04fbe6a218369858f653373c1
Wylliam Miguita <miguita@outlook.com>
2021-10-20 20:15:58 -0300

    Upgrading dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp        = require( 'gulp');
 var browserSync = require( 'browser-sync' ).create();
-var sass        = require( 'gulp-sass' );
+var sass        = require( 'gulp-sass' )(require('sass'));
 var njk         = require( 'gulp-nunjucks-render' );
 var gdata       = require( 'gulp-data' );
 var fs          = require( 'fs' );
